8cbfccf9d7125fda2e460d5bb24ddecc8771216c
maaike-slt <maaikesloot@home.nl>
2025-08-10 16:55:23 +0200

    added search function

--- cpp_00/ex01/PhoneBook.cpp
+++ cpp_00/ex01/PhoneBook.cpp
@@ -6,7 +6,7 @@
 /*   By: msloot <msloot@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/25 21:05:04 by msloot            #+#    #+#             */
-/*   Updated: 2025/08/08 17:19:18 by msloot           ###   ########.fr       */
+/*   Updated: 2025/08/10 16:53:59 by msloot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 PhoneBook::PhoneBook()
 {
 	this->amt = 0;
+	this->index = 0;
 }
 
 PhoneBook::~PhoneBook()
@@ -24,32 +25,42 @@ PhoneBook::~PhoneBook()
 bool	PhoneBook::input(std::string input)
 {
 	if (input == "ADD")
-	{
 		this->_add_contact();
-		if (this->amt < 8)
-			this->amt++;
-		return (true);
-	}
 	else if (input == "SEARCH")
-	{
-		// display contacts
-		return (true);
-	}
+		this->_search_contact();
 	else if (input == "EXIT")
 		return (false);
 	return (true);
 }
 
-bool	PhoneBook::_add_contact()
+void	PhoneBook::_add_contact()
 {
-	Contact	contact;
-	size_t	contact_index;
-
+	this->contact[this->index].read();
 	if (this->amt < 8)
-		contact_index = this->amt + 1;
-	else
-		contact_index = this->amt;
-	contact = this->contact[contact_index];
-	contact.read();
-	return (true);
+		this->amt++;
+	this->index = (this->index + 1) % 8;
+}
+
+void	PhoneBook::_search_contact()
+{
+	size_t	i;
+
+	if (this->amt == 0)
+	{
+		std::cout << "No contacts to search through" << std::endl;
+		return ;
+	}
+	i = 0;
+	while (i < this->amt)
+	{
+		std::cout << std::setw(10) << i + 1;
+		std::cout << "|";
+		std::cout << std::setw(10) << contact[i].first_name;
+		std::cout << "|";
+		std::cout << std::setw(10) << contact[i].last_name;
+		std::cout << "|";
+		std::cout << std::setw(10) << contact[i].nickname << std::endl;
+		i++;
+	}
+	
 }
--- cpp_00/ex01/PhoneBook.hpp
+++ cpp_00/ex01/PhoneBook.hpp
@@ -6,7 +6,7 @@
 /*   By: msloot <msloot@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/25 19:38:08 by msloot            #+#    #+#             */
-/*   Updated: 2025/08/08 17:01:14 by msloot           ###   ########.fr       */
+/*   Updated: 2025/08/10 16:54:30 by msloot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 #include <cstring>
 #include <unistd.h>
 #include <stdio.h>
+#include <iomanip>
 
 #include "Contact.hpp"
 
@@ -25,12 +26,14 @@ class PhoneBook
 private:
 	Contact	contact[8];
 	
-	bool	_add_contact();
+	void	_add_contact();
+	void	_search_contact();
 	public:
 	PhoneBook();
 	~PhoneBook();
 	
 	size_t	amt;
+	size_t	index;
 
 	bool	input(std::string input);
 };
